7a338245a314f62e4501764922283980a2674389
xuhao97 <1170180905@qq.com>
2018-03-20 23:53:10 +0800

    exe改变

---
+++ 新建文本文档.bat
@@ -0,0 +1,7 @@
+wc.exe -c -l -w testf1.c
+wc.exe -c -l -w testf1.c -o result.txt
+wc.exe -c -l -s D:\\workspace\\wordcount
+wc.exe -e stop.txt teste.c
+wc.exe -a test1.c
+wc.exe -c -l -w -e stop.txt -s D:\\workspace\\wordcount -o result.txt
+pause
